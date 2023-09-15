5ff6ce1bec2227b3c9e53b65ec9435bb2eb8a55c
zuryr <zuryr@proton.me>
2023-09-15 15:02:13 -0600

    cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Un modelo de generación de codigo SQL a partir de consultas en español',
+    author='Instituto Politecnico Nacional',
+    license='',
+)

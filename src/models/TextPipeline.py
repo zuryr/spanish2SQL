f8a6bf3c96edef0d40020a944f20a7553a390f54
Lance-24 <lancelote.ps@gmail.com>
2024-04-22 10:10:20 -0600

    Fix: reestructure of abstract class' attributes

--- src/models/TextPipeline.py
+++ src/models/TextPipeline.py
@@ -9,15 +9,12 @@ from src.models.SemanticEvaluator import SemanticEvaluator
 class TextPipeline(ABC):
     """Manages the behavior to perform a transformation to a text."""
 
-    def __init__(self, evaluator: SemanticEvaluator, extractor: SectionExtractor):
+    def __init__(self, evaluator: SemanticEvaluator):
         """
         Instantiates the pipeline.
         evaluator: SemanticEvaluator
-        extractor: SectionExtractor initialized with value rules
         """
-
         self.evaluator = evaluator
-        self.extractor = extractor
 
     @abstractmethod
     def transform_sections(self, section_list: list[Section]) -> list[Section | Condition]:
@@ -41,4 +38,19 @@ class TextPipeline(ABC):
         Returns:
             A cleaned section or a cleaned condition
         """
+        pass
+
+    @abstractmethod
+    def evaluate_table(self, section: Section) -> Section:
+        """Evaluate the TABLE in a section."""
+        pass
+
+    @abstractmethod
+    def evaluate_attribute(self, section: Section) -> Section:
+        """Evaluate the attribute in a section."""
+        pass
+
+    @abstractmethod
+    def evaluate_condition(self, section: Section) -> Condition:
+        """Evaluate the condition in a section."""
         pass

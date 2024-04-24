a2c5a01930f3ff33e7fa4e5050514f91551097bf
Lance-24 <lancelote.ps@gmail.com>
2024-04-24 14:05:34 -0600

    Fix simplePipeline

--- src/models/SimplePipeline.py
+++ src/models/SimplePipeline.py
@@ -9,10 +9,14 @@ from src.models.Enums.Classifications import Classifications
 class SimplePipeline(TextPipeline):
     """Semantic evaluator based on fixed rules."""
 
-    def __init__(self, evaluator: SemanticEvaluator, operator_extractor: SectionExtractor, value_extractor: SectionExtractor):
+    def __init__(self, evaluator: SemanticEvaluator, operator_extractor: SectionExtractor, value_extractor: SectionExtractor, operator: str, conditional_value: str, conditional_attribute: str):
         super().__init__(evaluator)
         self.operator_extractor = operator_extractor
         self.value_extractor = value_extractor
+        self.operator = operator
+        self.conditional_value = conditional_value
+        self.conditional_attribute = conditional_attribute
+
 
     def transform_sections(self, text: list[Section]) -> list[Section]:
         cleaned_sections = []
@@ -43,7 +47,6 @@ class SimplePipeline(TextPipeline):
     def evaluate_condition(self, section: Section, ATR_CONDITION: str, VAL_CONDITION: str) -> Condition:
         """Extract operator, conditional_value and conditional_atribute """
         
-        # TODO: extract operators
         operators = self.operator_extractor.extract_exact_match(section.text)
         
         # Assume that the value extractor contains rules with
@@ -52,25 +55,19 @@ class SimplePipeline(TextPipeline):
 
         atribute = "ATR_CONDIcION"
         value = "VALOR"
-        #TODO: filter by conditional value and conditional attribute
+
         conditional_value = [section for section in extracted_values if section.clasification == atribute] 
         conditional_atribute = [section for section in extracted_values if section.clasification == value] 
-
-        # TODO: generate condition
-        condition = Condition()
      
-        if operators:
-            first_operator = operators[0]
-        if conditional_value:
-            first_conditional_value = conditional_value[0]
-        if conditional_atribute:
-            first_conditional_atribute = conditional_atribute[0]
+        first_operator = operators[0]
+        first_conditional_value = conditional_value[0]
+        first_conditional_atribute = conditional_atribute[0]
 
-        condition.operator = first_operator
-        condition.conditional_value = first_conditional_value
-        condition.conditional_attribute = first_conditional_atribute
+        self.operator = first_operator
+        self.conditional_value = first_conditional_value
+        self.conditional_attribute = first_conditional_atribute
 
-        condition = Condition(first_operator, first_conditional_value, first_conditional_atribute)
+        return Condition(first_operator, first_conditional_value, first_conditional_atribute)
 
-        return condition
+        
    
